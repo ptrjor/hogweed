f8e16c946e043e29ccc744726e2bec4ac7e07da0
ptrjor <ptrjor@gmail.com>
2021-10-31 19:08:57 +0100

    Petter 2-player lokal testing
    Har skrevet en del kommentarer i fila. Dette er min foreløpige testing som ikke fungerer optimalt ennå, men jeg tror det går framover :)

--- Example vanilla project/script.js
+++ Example vanilla project/script.js
@@ -1,12 +1,18 @@
-var myGamePiece;
+var myGamePiece, myGamePiece2, myGamePiece3;
 var myObstacles = [];
 var myScore;
 var button1Held = false;
+var button2Held = false;
+var button3Held = false;
+var p1key = "KeyQ";
+var p2key = "KeyP";
+var type;
 const gravity = 0.1;
 
 function startGame() {
-    myGamePiece = new component(60, 30, "blue", 10, 540);
-    // myGamePiece.gravity = 0.05; // let g be global constant
+    myGamePiece = new component(80, 50, "blue", 10, 540, "daxtrot");
+    myGamePiece2 = new component(30, 20, "red", 10, 490, "player");  //
+    myGamePiece3 = new component(30, 20, "green", 60, 490, "player");
     myScore = new component("30px", "Consolas", "black", 40, 40, "text");
     myGameArea.start();
 }
@@ -36,6 +42,7 @@ function component(width, height, color, x, y, type) {
     this.speedY = 0;    
     this.x = x;
     this.y = y;
+
     this.update = function() {
         ctx = myGameArea.context;
         if (this.type == "text") {
@@ -45,16 +52,31 @@ function component(width, height, color, x, y, type) {
         } else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
+
         }
     }
     this.newPos = function() {
         this.x += this.speedX;
-        this.y += this.speedY // + this.gravitySpeed;
+        this.y += this.speedY; 
         this.applyGravity();
     }
     this.onGround = function() {
+
 	// return true if this is on the ground
+        
         var rockbottom = myGameArea.canvas.height - this.height;
+        if(this.type === "player")
+        {
+            /* Her settes "rockbottom" for playerobjektene cirka til y-verdien til daxtrot, altså mygamepiece.y. 
+             når playerobjektene treffer denne blir de reseta til deres standard y-verdi som du ser under testing. 
+             Den bør vel heller settes til y-verdien til daxtrot, eller noe liknende. Ærlig talt litt for mye testing fram 
+             og tilbake her uten å ha en helt krystallklar plan for hva ting bør gjøre. Derfor sier jeg det er rotete - så du 
+             får gjøre hva du vil ut av det. Gjerne re-design dette om du synes dette blir feil. Veldig usikker selv. */
+
+            rockbottom = myGamePiece.y - myGamePiece.height/2;
+          
+            
+        }
         if (this.y >= rockbottom) {
 	    return true;
 	}
@@ -63,16 +85,32 @@ function component(width, height, color, x, y, type) {
 	}
     }
 
+   // console.log(this.onGround() + " type: " + this.type)
+
     this.applyGravity = function() {
         if (this.onGround()) {
-	    this.y = myGameArea.canvas.height - this.height;
+        //console.log("piece on ground: " + color)
+	    this.y = y - this.height;
 	    this.speedY = 0;
         }
 	else { // piece is in the air, apply gravity
 	    if (this.y < 1) { // make sure we don't leave screen at top
 		this.y = 1;
 	    }
-	    accelY(-gravity);
+        
+        //dette må ryddes opp i. for mye if-else, og å sjekke etter color er ikke helt optimalt. var bare løsninga jeg kom på først under testing
+        if(color == "red")
+        {
+            accelY(myGamePiece2,-gravity);
+        }
+        else if(color == "green")
+        {
+            accelY(myGamePiece3,-gravity);
+        }
+        else
+        {
+            accelY(myGamePiece,-gravity);
+        }
 	}
     }
 
@@ -125,17 +163,27 @@ function updateGameArea() {
     myScore.update();
     myGamePiece.newPos();
     myGamePiece.update();
+    myGamePiece2.newPos();
+    myGamePiece2.update();
+    myGamePiece3.newPos();
+    myGamePiece3.update();
     // check button status after update
-    if (button1Held & myGamePiece.onGround()) {
-	console.log("button 1 is held!")
+    if (button3Held && button2Held && myGamePiece.onGround()) {
+	//console.log("button 1 is held!")
 	myGamePiece.speedX = 1;
+    myGamePiece2.speedX = 1;
+    myGamePiece3.speedX = 1;
     }
     else if (myGamePiece.onGround()) {
 	if (myGamePiece.x > 10) {
 	    myGamePiece.speedX = -1;
+        myGamePiece2.speedX = -1;
+        myGamePiece3.speedX = -1;
 	}
 	else {
 	    myGamePiece.speedX = 0;
+        myGamePiece2.speedX = 0;
+        myGamePiece3.speedX = 0;
 	}
     }
 }
@@ -145,33 +193,72 @@ function everyinterval(n) {
     return false;
 }
 
-function butDown() {
-    console.log("Pressed button 1");
-    button1Held = true;
+function butDown(keycode) {
+   //Her lagres verdi for om en knapp holdes nede
+    if (keycode == "Space") {
+        button1Held = true;
+    }
+    else if(keycode == p1key) {
+        button2Held = true;
+    }
+    else if(keycode == p2key) {
+        button3Held = true;
+    } 
 }
 
+
+//Kjøres om en av de designerte spilleknappene er trykt og slippes (keyup) 
 function butUp() {
-    console.log("Released button 1");
+
+    if (button2Held && button3Held && myGamePiece.onGround()) {
+        accelY(myGamePiece, 5);        
+        } 
+    else if(button2Held)
+    {
+        if(myGamePiece2.onGround())
+        {
+            accelY(myGamePiece2, 6);
+              
+        }
+    }
+    else if(button3Held)
+    {
+        if(myGamePiece3.onGround())
+        {
+            accelY(myGamePiece3, 6);    
+        }
+        
+    }
+
+    //Dette kan sikkert optimaliseres også
     button1Held = false;
-    if (myGamePiece.onGround()) {
-	console.log("Jump!");
-	accelY(5);
-    }
+    button2Held = false;
+    button3Held = false;
+
 }
 
-function accelY(n) {
-    myGamePiece.speedY -= n;
+//Hoppefunksjon
+function accelY(piece, n) {
+    piece.speedY -= n;
 }
 
+function accelX(piece, n) {
+    piece.speedX += n;
+}
+
+//Keydown-sjekker
 document.addEventListener('keydown', (event) => {
-    if (event.code == "Space") {
-	butDown();
+    butDown(event.code);
+}, false);  
+
+//Keyup-sjekker
+document.addEventListener('keyup', (event) => {
+    if (event.code == "Space" || p1key || p2key) {
+	butUp(event.code);
     }
 }, false);
 
-document.addEventListener('keyup', (event) => {
-    if (event.code == "Space") {
-	butUp();
-    }
-}, false);
+
+
+
 
